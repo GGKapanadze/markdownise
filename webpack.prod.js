3bb6fe10d9e422c067fe510023584eefe9055b2c
GGKapanadze <kapanadzeg41@gmail.com>
2020-06-07 15:32:01 +0400

    :bug: Fix config.

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,6 @@ const glob = require("glob");
 const merge = require("webpack-merge");
 const mainConfig = require("./webpack.config.js");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const PurgecssPlugin = require("purgecss-webpack-plugin");
 const TerserJSPlugin = require("terser-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
